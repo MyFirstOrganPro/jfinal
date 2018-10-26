7e0dbd069b9b31d25d0d30b1518017c528a0ec6e
jipsonliang <39261943+jipsonliang@users.noreply.github.com>
2018-10-26 15:50:41 +0800

    单独push测试

--- src/com/demo/Start.java
+++ src/com/demo/Start.java
@@ -3,7 +3,10 @@ package com.demo;
 import com.jfinal.core.JFinal;
 
 public class Start {
-
+/**
+ * 改了提交，测试单独push
+ * @param args
+ */
 	public static void main(String[] args) {
 		JFinal.start("WebContent", 8082, "/", 5);
 	}
